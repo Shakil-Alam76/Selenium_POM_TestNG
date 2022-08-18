a195c73e529c8aa39ddf0dabc77da06a3dd60de8
Md.Shakil Alam <shakil.shanto76@gmail.com>
2022-08-18 11:20:20 +0600

    Solve testng library add issue

--- src/test/java/Setup.java
+++ src/test/java/Setup.java
@@ -1,12 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+
 public class Setup {
     public WebDriver driver;
+
+WebDriverWait wait;
+
     @BeforeTest
     public void setUp() throws IOException {
         ChromeOptions ops = new ChromeOptions();
@@ -14,7 +21,8 @@ public class Setup {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
         driver = new ChromeDriver(ops);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
     @AfterTest
     public void logout(){
@@ -22,4 +30,4 @@ public class Setup {
     }
 }
 
-}
+
